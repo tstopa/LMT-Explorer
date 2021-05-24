43a40167239a7d460c8c7acf748353a9eff843e8
Kacper Szot <kacperszot1@gmail.com>
2021-05-24 09:55:04 +0200

    add dummy help and about menu

--- src/main.js
+++ src/main.js
@@ -1,74 +1,86 @@
 const {
-	app,
-	BrowserWindow,
-	dialog,
-	ipcMain,
-	Menu,
-	MenuItem,
+  app,
+  BrowserWindow,
+  dialog,
+  ipcMain,
+  Menu,
+  MenuItem,
 } = require('electron')
 const path = require('path')
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
-	// eslint-disable-line global-require
-	app.quit()
+  // eslint-disable-line global-require
+  app.quit()
 }
 let mainWindow = null
 
 const createWindow = () => {
-	// Create the browser window.
-	mainWindow = new BrowserWindow({
-		width: 1200,
-		height: 800,
-		resizable: false,
-		webPreferences: {
-			nodeIntegration: true,
-			contextIsolation: false,
-		},
-		icon: path.join(__dirname, '/../../icons/png/64x64.png'),
-	})
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    icon: path.join(__dirname, '/../../icons/png/64x64.png'),
+  })
 
-	// setting up the menu with just two items
-	const menu = Menu.buildFromTemplate([
-		{
-			label: 'File',
-			submenu: [
-				{
-					label: 'Open File',
-					accelerator: 'CmdOrCtrl+O',
-					// this is the main bit hijack the click event
-					click() {
-						// construct the select file dialog
-						dialog
-							.showOpenDialog({
-								properties: ['openFile'],
-							})
-							.then(function (fileObj) {
-								// the fileObj has two props
-								if (!fileObj.canceled) {
-									mainWindow.webContents.send(
-										'open-file-request-response',
-										fileObj.filePaths[0]
-									)
-								}
-							})
-							// should always handle the error yourself, later Electron release might crash if you don't
-							.catch(function (err) {})
-					},
-				},
-				{
-					label: 'Exit',
-					click() {
-						app.quit()
-					},
-				},
-			],
-		},
-	])
-	Menu.setApplicationMenu(menu)
-	// and load the index.html of the app.
-	mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
-	// Open the DevTools.
-	mainWindow.webContents.openDevTools()
+  // setting up the menu with just two items
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Open File',
+          accelerator: 'CmdOrCtrl+O',
+          // this is the main bit hijack the click event
+          click() {
+            // construct the select file dialog
+            dialog
+              .showOpenDialog({
+                properties: ['openFile'],
+              })
+              .then(function (fileObj) {
+                // the fileObj has two props
+                if (!fileObj.canceled) {
+                  mainWindow.webContents.send(
+                    'open-file-request-response',
+                    fileObj.filePaths[0]
+                  )
+                }
+              })
+              // should always handle the error yourself, later Electron release might crash if you don't
+              .catch(function (err) {})
+          },
+        },
+        {
+          label: 'Exit',
+          click() {
+            app.quit()
+          },
+        },
+      ],
+    },
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'Manual',
+          accelerator: 'CmdOrCtrl+h',
+        },
+        {
+          label: 'About',
+        },
+      ],
+    },
+  ])
+  Menu.setApplicationMenu(menu)
+  // and load the index.html of the app.
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -82,63 +94,63 @@ app.on('ready', createWindow)
 
 const visualizationCtxMenu = new Menu()
 visualizationCtxMenu.append(
-	new MenuItem({
-		label: 'show selected products context',
-		click: () => mainWindow.webContents.send('show-selected-products'),
-	})
+  new MenuItem({
+    label: 'show selected products context',
+    click: () => mainWindow.webContents.send('show-selected-products'),
+  })
 )
 visualizationCtxMenu.append(
-	new MenuItem({
-		label: 'show all products context',
-		click: () => mainWindow.webContents.send('show-all-products'),
-	})
+  new MenuItem({
+    label: 'show all products context',
+    click: () => mainWindow.webContents.send('show-all-products'),
+  })
 )
 
 ipcMain.on('pop-visualization-ctx-menu', (evnt, args) => {
-	visualizationCtxMenu.popup()
+  visualizationCtxMenu.popup()
 })
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit()
-	}
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 const showError = (error) => {
-	dialog.showMessageBox(null, {
-		type: 'error',
-		buttons: ['ok'],
-		defaultId: 2,
-		title: 'Error',
-		message: 'error occured ',
-		detail: error,
-	})
+  dialog.showMessageBox(null, {
+    type: 'error',
+    buttons: ['ok'],
+    defaultId: 2,
+    title: 'Error',
+    message: 'error occured ',
+    detail: error,
+  })
 }
 ipcMain.on('show-error', (event, args) => {
-	showError(args)
+  showError(args)
 })
 ipcMain.on('open-file-request', (evnt, args) => {
-	dialog
-		.showOpenDialog({
-			properties: ['openFile'],
-		})
-		.then((fileObj) => {
-			// the fileObj has two props
-			if (!fileObj.canceled) {
-				mainWindow.webContents.send(
-					'open-file-request-response',
-					fileObj.filePaths[0]
-				)
-			}
-		})
-		// should always handle the error yourself, later Electron release might crash if you don't
-		.catch(function (err) {})
+  dialog
+    .showOpenDialog({
+      properties: ['openFile'],
+    })
+    .then((fileObj) => {
+      // the fileObj has two props
+      if (!fileObj.canceled) {
+        mainWindow.webContents.send(
+          'open-file-request-response',
+          fileObj.filePaths[0]
+        )
+      }
+    })
+    // should always handle the error yourself, later Electron release might crash if you don't
+    .catch(function (err) {})
 })
 app.on('activate', () => {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (BrowserWindow.getAllWindows().length === 0) {
-		createWindow()
-	}
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
 })
 
 // In this file you can include the rest of your app's specific main process
